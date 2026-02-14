ebac44d64fd65cbbcdb3dcceef353941f9ca20be
Serhii Kusyi <sergijkusij@gmail.com>
2026-02-14 16:14:20 +0200

    add new titles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 export const App = () => {
-  return <h1>Mobile App</h1>;
+  return (
+    <>
+      <h1>Group project - test</h1>
+      <h2>Group project - test</h2>
+      <h3>Group project - test</h3>
+      <h4>Group project - test</h4>
+    </>
+  );
 };
-// test
